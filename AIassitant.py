f28b14d5ff96ecbdc989a491835bbb9fc4a1c30a
Lovely Varshney <lovelyvarshney2001@gmail.com>
2024-02-27 13:02:49 +0530

    Changes 2

--- AIassitant.py
+++ AIassitant.py
@@ -1,42 +1,34 @@
-import pyttsx3  # pip install pyttsx3   -----------pyttsx3 is a text-to-speech conversion library in Python. 
-import speech_recognition as sr  # pip install speechRecognition        --- for taking input from microphone
+import pyttsx3
+import speech_recognition as sr
 import datetime
 import wikipedia
 import webbrowser
 import os
+import random
 
-engine = pyttsx3.init('sapi5')                  #sapi5-speech API -- for windows-- PROVIDED BY MICROSSOFT
+engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)                           #2VOICES IN SYSTEM - MALE,FEAMLE
-engine.setProperty('voice', voices[0].id)     
-
+engine.setProperty('voice', voices[0].id)
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour >= 0 and hour < 12:
+    if 0 <= hour < 12:
         speak("Good Morning!")
-
-    elif hour >= 12 and hour < 18:
+    elif 12 <= hour < 18:
         speak("Good Afternoon!")
-
     else:
         speak("Good Evening!")
-
-    speak("Hello I am Jarvis , How may I help you?")
-
+    speak("Hello, I am Jarvis. How may I help you?")
 
 def takeCommand():
-    # It takes microphone input from the user and returns string output
-
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 1                      #seconds of non-speaking audio before a phrase is considered complete
+        r.pause_threshold = 1
         audio = r.listen(source)
 
     try:
@@ -45,37 +37,59 @@ def takeCommand():
         print(f"User said: {query}\n")
 
     except Exception as e:
-        print("Say that again please...")
+        print("Say that again, please...")
         return "None"
-    return query
+    return query.lower()
 
+def openWebsite(url):
+    webbrowser.open(url)
+
+def searchOnWikipedia(query):
+    speak('Searching Wikipedia...')
+    results = wikipedia.summary(query, sentences=2)
+    speak("According to Wikipedia")
+    print(results)
+    speak(results)
+
+def playMusic():
+    music_dir = 'C:\\Users\\akash\\Music\\fvt'
+    songs = os.listdir(music_dir)
+    if songs:
+        random_song = random.choice(songs)
+        print(f"Now playing: {random_song}")
+        os.startfile(os.path.join(music_dir, random_song))
+    else:
+        speak("No music files found in the specified directory.")
 
 if __name__ == "__main__":
     wishMe()
 
-while True:
-        query = takeCommand().lower()
+    while True:
+        query = takeCommand()
 
-        # Logic for executing tasks based on query
         if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=5)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
+            search_query = query.replace("wikipedia", "")
+            searchOnWikipedia(search_query)
+
         elif 'open moodle' in query:
-            webbrowser.open("moodle.dbit.in")
+            openWebsite("http://moodle.dbit.in")
+
         elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
+            openWebsite("http://youtube.com")
+
         elif 'open google' in query:
-            webbrowser.open("google.com")
+            openWebsite("http://google.com")
+
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            print(strTime)
             speak(f"Sir, the time is {strTime}")
+
         elif 'play music' in query:
-            music_dir = 'C:\\Users\\akash\\Music\\fvt'
-            songs = os.listdir(music_dir)
-            print(songs)
-            os.startfile(os.path.join(music_dir, songs[4]))
+            playMusic()
+
+        elif 'stop' in query or 'exit' in query:
+            speak("Goodbye!")
+            break
+
+        else:
+            speak("I'm sorry, I didn't understand that. Can you please repeat?")
